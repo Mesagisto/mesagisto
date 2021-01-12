03de07cb954d8329236de4cb153ad16aabb674ab
Itsusinn奕䜣 <itsusinn@foxmail.com>
2021-01-12 16:42:05 +0800

    feat(Extension): move

---
+++ discord-msg-source/src/main/kotlin/io/github/itsusinn/extension/test/Timing.kt
@@ -0,0 +1,22 @@
+package io.github.itsusinn.extension.test
+
+import io.github.itsusinn.extension.jackson.jacksonLogger
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {  }
+
+fun timing(
+   times:Int = 1,
+   log:Boolean = true,
+   block:()->Unit,
+):Long{
+   val start = System.currentTimeMillis()
+   for (i in 1..times){
+      block.invoke()
+   }
+   val end = System.currentTimeMillis()
+   if (log){
+      jacksonLogger.debug("""${block} invoke $times times cost ${end-start}""")
+   }
+   return end-start
+}
