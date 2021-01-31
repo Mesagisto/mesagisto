6459b8dd05de9aade965b92e9f4ac9ab9ce06392
Itsusinn奕䜣 <itsusinn@foxmail.com>
2021-01-31 22:07:32 +0800

    Initial commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,9 @@
 rootProject.name = "easy-forward"
 include("message-dispatcher")
 include("mirai-message-source")
+include("discord-message-source")
+include("test-message-source")
+include("telegram-message-source")
 include("forward-client:okhttp-client")
 findProject(":forward-client:okhttp-client")?.name = "okhttp-client"
 include("minecraft-message-source:bukkit-message-source")
